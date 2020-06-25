37e2375faee0cddf961a3d18568f3cc91618ae0c
Alexey Andrianov <Alexey.Andrianov@student.ie.edu>
2020-06-25 23:30:56 +0200

    Modified endpoint name

--- src/main/java/com/watson/boot/controller/BootController.java
+++ src/main/java/com/watson/boot/controller/BootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BootController {
 
-    @GetMapping
+    @GetMapping("chatbot")
     public String chatbot() {
         return "chatbot";
     }
